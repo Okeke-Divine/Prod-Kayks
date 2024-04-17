4e62bb3f8ad2a622caf4e341e360b319bc9074e4
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 20:06:21 +0100

    Updated the meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Prod. Kayks | Music Producer",
+  description: "Get industry-ready instrumentals to fuel your next track."
 };
 
 export default function RootLayout({ children }) {
@@ -17,9 +18,11 @@ export default function RootLayout({ children }) {
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.3.0/uicons-thin-rounded/css/uicons-thin-rounded.css' />
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.3.0/uicons-brands/css/uicons-brands.css' />
         {/* facebook */}
-        <meta name="og:title" content="Prod. Kayks | Music Producer" />
+        <meta property="og:title" content="Prod. Kayks | Music Producer" />
+        <meta property="og:description" content="Get industry-ready instrumentals to fuel your next track." />
         {/* twitter */}
         <meta name="twitter:title" content="Prod. Kayks | Music Producer" />
+        <meta property="twitter:description" content="Get industry-ready instrumentals to fuel your next track." />
       </head>
       <body className={inter.className + ' ' + 'bg-purple-dark text-white'}>
         <Navbar />
