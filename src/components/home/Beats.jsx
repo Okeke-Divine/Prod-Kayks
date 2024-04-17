1b5512f46da6d1f0f9648428b53aa8d7b09acfc3
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 20:01:18 +0100

    Updated the beats section

--- src/components/home/Beats.jsx
+++ src/components/home/Beats.jsx
@@ -13,8 +13,60 @@ export default function Beats() {
         </div>
         <div className="my-5 font-bold text-4xl text-center">Beats</div>
 
-        <div className="">
+        {/* afro */}
+        <div className="mb-5">
+          <ComponentTitle name="Afro Beats" url="/beats/?genre=afro-beats" />
+          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+          </div>
+        </div>
+        {/* amapiano */}
+        <div className="mb-5">
+          <ComponentTitle name="Amapiano" url="/beats/?genre=amapiano" />
+          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+          </div>
+        </div>
+        {/* drills */}
+        <div className="mb-5">
           <ComponentTitle name="Drills" url="/beats/?genre=drill" />
+          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+            <BeatCard />
+          </div>
+        </div>
+        {/* traps */}
+        <div className="mb-5">
+          <ComponentTitle name="Traps" url="/beats/?genre=trap" />
           <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
             <BeatCard />
             <BeatCard />
