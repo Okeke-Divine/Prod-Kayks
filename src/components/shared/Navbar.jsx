c38cf4cdc97a78252c9d8dae397368d4c840710f
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 21:49:58 +0100

    fixed minor bugs

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -13,6 +13,9 @@ export default function Navbar() {
             </div>
           </Link>
           <div className="fixed md:static z-50 md:z-1 bg-purple-dark-2 md:bg-transparent w-full md:w-fit top-0 left-0 p-5 md:p-0 block md:flex items-center gap-x-5">
+            <div>
+            <i className="fi fi-tr-circle-xmark"></i>
+            </div>
             <div><Link href="/">Home</Link></div>
             <div><Link href="/search">Search</Link></div>
             <div><Link href="/releases">Releases</Link></div>
@@ -27,7 +30,7 @@ export default function Navbar() {
             <button className="bg-pink rounded-md py-2 px-3">All Beats</button>
           </Link>
           <button className="block md:hidden p-2 rounded-lg border-2 duration-300 border-transparent hover:border-red-300">
-            <i class="relative top-[2px] fi fi-ts-bars-staggered"></i>
+            <i className="relative top-[2px] fi fi-ts-bars-staggered"></i>
           </button>
         </div>
       </div>
