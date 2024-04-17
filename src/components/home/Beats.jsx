c037f775f6934571c82831a3daec4f7ba6e3d1d7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 20:13:22 +0100

    updated the beats card

--- src/components/home/Beats.jsx
+++ src/components/home/Beats.jsx
@@ -18,68 +18,22 @@ export default function Beats() {
           <ComponentTitle name="Afro Beats" url="/beats/?genre=afro-beats" />
           <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
             <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
           </div>
         </div>
         {/* amapiano */}
         <div className="mb-5">
           <ComponentTitle name="Amapiano" url="/beats/?genre=amapiano" />
-          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-          </div>
+
         </div>
         {/* drills */}
         <div className="mb-5">
           <ComponentTitle name="Drills" url="/beats/?genre=drill" />
-          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-          </div>
+         
         </div>
         {/* traps */}
         <div className="mb-5">
           <ComponentTitle name="Traps" url="/beats/?genre=trap" />
-          <div className="flex flex-nowrap whitespace-nowrap overflow-x-auto gap-3 pb-2">
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-            <BeatCard />
-          </div>
+
         </div>
       </div>
       <br />
--- src/components/shared/BeatCard.jsx
+++ src/components/shared/BeatCard.jsx
@@ -1,16 +1,16 @@
 import CTag from "../shared/CTag";
-export default function BeatCard() {
+export default function BeatCard({ title, description, genre, imgUrl }) {
   return (
     <>
       <div className="w-[250px] p-2 rounded-lg border-2 border-white">
-        <div className="w-full h-[120px] bg-blue-200 rounded-lg mb-3">
-
-        </div>
+        <div className="w-full h-[120px] bg-blue-200 rounded-lg mb-3"></div>
         <div>
-          <CTag name="Drill" />
+          <CTag name={genre} />
         </div>
-        <div className="font-bold mt-1">Darkness</div>
-        <div className="text-gray-400 break-words text-wrap w-[250px]">Metro Boomin x Central Cee x Pop Smoke type beat</div>
+        <div className="font-bold mt-1">{title}</div>
+        <div className="text-gray-400 break-words text-wrap w-[250px]">
+          {description}
+        </div>
       </div>
     </>
   );
