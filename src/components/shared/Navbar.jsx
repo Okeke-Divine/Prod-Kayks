970d6f8045be5b9f029d08d8eb603911c7be01b7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 21:38:00 +0100

    updated

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,7 +2,6 @@ import Divider from "./Divider";
 import Link from "next/link";
 
 export default function Navbar() {
-
   return (
     <>
       <div className="flex px-10 py-5 justify-between items-center">
@@ -20,11 +19,16 @@ export default function Navbar() {
             <Link href="/about">About</Link>
           </div>
         </div>
-        <div className="w-fit flex gap-x-5 items-center">
-          <Link href="#" className="hidden">Sign up</Link>
+        <div className="w-fit flex gap-x-3 items-center">
+          <Link href="#" className="hidden">
+            Sign up
+          </Link>
           <Link href="/beats">
             <button className="bg-pink rounded-md py-2 px-3">All Beats</button>
           </Link>
+          <button className="p-2 rounded-lg border-2 border-red-300">
+            <i class="relative top-[2px] fi fi-ts-bars-staggered"></i>
+          </button>
         </div>
       </div>
       <Divider />
