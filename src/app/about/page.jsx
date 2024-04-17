5415701c876b6574e9ca95762270515a1708d910
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-17 20:39:27 +0100

    udpated the search/aabout page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,9 @@
+import Hero from "../../components/about/Hero"
+
 export default function about() {
   return (
     <>
-      <div className="w-full min-h-[80vh] justify-center items-center flex font-bold text-4xl">
-        Coming soon...
-      </div>
+      <Hero />
     </>
   );
 }
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,7 +1,9 @@
+import Hero from "../../components/about/Hero"
+
 export default function Search(){
     return (
         <>
-          <div className="w-full min-h-[80vh] justify-center items-center flex font-bold text-4xl">Coming soon...</div>
+      <Hero />
         </>
       );
 }
