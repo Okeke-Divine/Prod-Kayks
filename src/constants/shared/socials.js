6386a13b8e61f033effc476e8ac4c2ad6f9e7164
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-18 21:11:43 +0100

    updated the constants

--- src/constants/shared/socials.js
+++ src/constants/shared/socials.js
@@ -4,3 +4,4 @@ export const TIKTOK_URL = "https://tiktok.com/@prod.kayks";
 export const INSTAGRAM_URL = "https://www.instagram.com/prod.kayks/";
 export const YOUTUBE_URL = "https://www.youtube.com/@prodkayks_beats";
 export const SPOTIFY_URL = "https://open.spotify.com/artist/68NmcvZmHz8kYBnTQ4KsUp";
+export const WHATSAPP_URL = "https://wa.me/+2347064878285";
