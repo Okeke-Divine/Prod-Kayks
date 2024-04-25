7b7bc034bbae824923fc5d9018141c79dff21c56
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-25 22:17:15 +0100

    Updated componenttitle

--- src/components/shared/ComponentTitle.jsx
+++ src/components/shared/ComponentTitle.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ComponentTitle({name, url}) {
   return (
     <>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mb-2">
         <div className="font-bold text-2xl">{name}</div>
         <div>
           <Link className="text-wood" href={url}>
