56389ab558c82ebfee27e4de4a0f679a637721a9
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-25 17:24:11 +0100

    Updated the about page

--- src/components/about/Hero.jsx
+++ src/components/about/Hero.jsx
@@ -1,10 +1,23 @@
+import {
+  TWITTER_URL,
+  TIKTOK_URL,
+  INSTAGRAM_URL,
+  YOUTUBE_URL,
+  FACEBOOK_URL,
+} from "../../constants/shared/socials";
 import Image from "next/image";
 import Link from "next/link";
 import Divider from "../shared/Divider";
 
-import { SPOTIFY_URL } from "../../constants/shared/socials";
-
 export default function Hero() {
+  const socials = [
+    { name: "Tiktok", url: TIKTOK_URL },
+    { name: "Instagram", url: INSTAGRAM_URL },
+    { name: "Youtube", url: YOUTUBE_URL },
+    { name: "Twitter", url: TWITTER_URL },
+    { name: "Facebook", url: FACEBOOK_URL },
+  ];
+
   return (
     <>
       <div className="relative">
@@ -32,8 +45,27 @@ export default function Hero() {
                 <div className="text-wood text-center mt-2">
                   Get industry-ready instrumentals to fuel your next track.
                 </div>
-                <div className="mt-5">
-                  
+                <div className="mt-4 flex gap-x-5">
+                  <div>
+                    <Link href={socials[0].url} target="_blank">
+                      <i className="footerIcon fi fi-brands-tik-tok"></i>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link href={socials[1].url} target="_blank">
+                      <i className="footerIcon fi fi-brands-instagram"></i>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link href={socials[2].url} target="_blank">
+                      <i className="footerIcon fi fi-brands-youtube"></i>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link href={socials[4].url} target="_blank">
+                      <i className="footerIcon fi fi-brands-facebook"></i>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
