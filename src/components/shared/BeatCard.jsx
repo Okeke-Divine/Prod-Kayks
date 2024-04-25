cbfc02bf1a05ea6294ca7762cbfe0168f9c9cf0c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-25 22:02:43 +0100

    Updated the beat card component

--- src/components/shared/BeatCard.jsx
+++ src/components/shared/BeatCard.jsx
@@ -24,10 +24,10 @@ export default function BeatCard({
     <>
       <div className="w-[250px] h-fit p-2 rounded-lg border-2 border-white">
         {/* image container */}
-        <div className="w-full h-[220px] bg-blue-200 rounded-lg mb-3 overflow-hidden relative">
+        <div className="w-full h-[220px] bg-wood rounded-lg mb-3 overflow-hidden relative">
           <img
             className="absolute inset-0 object-cover w-full h-full"
-            src={imgUrl}
+            // src={imgUrl}
             alt={title}
           />
         </div>
