709c360fa37f38e0d994eb32eba2cff4726794ce
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 00:28:00 +0100

    Updated the beat form

--- src/app/private/admin/beat/new/page.jsx
+++ src/app/private/admin/beat/new/page.jsx
@@ -1,18 +1,23 @@
 export default function NewBeat() {
   async function createBeat(data) {
-    "use server"
+    "use server";
 
-    console.log(data)
+    console.log(data);
   }
   return (
     <>
       <div className="mainLayout">
         <h1 className="text-2xl">Beat | New</h1>
         <form action={createBeat} className="mt-5">
-            <div>
-                <div className="adminInputContainer"><input name="" className="adminInput" /></div>
+          <div>
+            <div className="adminInputContainer">
+              <input name="name" placeholder="Beat name" className="adminInput" />
             </div>
-            <button className="bg-pink py-2 px-5 rounded mt-3">Submit</button>
+            <div className="adminInputContainer">
+              <input name="code" placeholder="Beat code" className="adminInput" />
+            </div>
+          </div>
+          <button className="bg-pink py-2 px-5 rounded mt-3">Submit</button>
         </form>
       </div>
     </>
