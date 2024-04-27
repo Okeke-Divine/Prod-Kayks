4c4aff198b8c04d4546edf265a70acff2c5ed035
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-27 21:19:40 +0100

    Finalized the list genre admin page

--- src/app/private/admin/genre/page.jsx
+++ src/app/private/admin/genre/page.jsx
@@ -6,17 +6,25 @@ async function getGenres() {
 }
 
 export default async function Genre() {
-  const genres = await getGenres();
+  const genres = await getGenres({select: {name: true, thumbnail_url:true}});
 
   return (
     <>
       <div className="mainLayout">
         <h1 className="text-2xl">Genres | List</h1>
-        <table>
+        <table className="table w-full mt-5">
           <tr>
-            <th>S/N</th>
-            <th></th>
+            <th className="adminTableRow">S/N</th>
+            <th className="adminTableRow">Name</th>
+            <th className="adminTableRow">Thumbnail URL</th>
           </tr>
+            {genres.map((genre, index) => (
+                <tr>
+                    <th className="adminTableRow">{(index+1)}</th>
+                    <th className="adminTableRow">{genre.name}</th>
+                    <th className="adminTableRow">{genre.thumbnail_url}</th>
+                </tr>
+            ))}
         </table>
       </div>
     </>
