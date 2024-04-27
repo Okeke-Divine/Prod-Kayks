d149717b8438b4d68ba0ca2f817bdda505026c71
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-27 21:05:52 +0100

    Major update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Hero from "../components/home/Hero"
 import Services from "../components/home/Services"
 import Beats from "../components/home/Beats"
-import Genres from "../components/home/Genres.tsx"
+import Genres from "../components/home/Genres"
 
 export default function Home() {
   return (
--- src/components/home/Genres.jsx
+++ src/components/home/Genres.jsx
@@ -17,11 +17,19 @@ export default async function Genres() {
           <CTag name="Genres" />
         </div>
         <div className="mt-5 font-bold text-4xl text-center">Genres</div>
-        <div className="mb-5">
+        <div className="my-5 grid grid-cols-1 md:grid-cols-4 gap-2">
           {genres.map((genre, index) => (
-            <>
-              <div>{genre.name}</div>
-            </>
+            <div
+              key={index}
+              className="rounded flex justify-center items-center border-2 border-[rgb(255,215,128)] cursor-pointer"
+            >
+              <div className="w-full">
+                <div className="bg-wood w-full h-[150px] rounded">
+                  {/* image container */}
+                </div>
+                <div className="p-1">{genre.name}</div>
+              </div>
+            </div>
           ))}
         </div>
       </div>
