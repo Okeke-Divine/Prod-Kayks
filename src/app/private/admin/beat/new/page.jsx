2b039f54a4d3a4fd4238f53df1e0bf6567a02a64
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 00:35:44 +0100

    Updated the admin form

--- src/app/private/admin/beat/new/page.jsx
+++ src/app/private/admin/beat/new/page.jsx
@@ -11,10 +11,74 @@ export default function NewBeat() {
         <form action={createBeat} className="mt-5">
           <div>
             <div className="adminInputContainer">
-              <input type="text" name="name" placeholder="Beat name" className="adminInput" required />
+              <label className="adminInputLabel">Name</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="adminInput"
+              />
             </div>
             <div className="adminInputContainer">
-              <input type="number" name="code" placeholder="Beat code" className="adminInput" />
+              <label className="adminInputLabel">Select a Genre</label>
+              <select className="adminInput">
+                <option default disabled>
+                  Select a Genre
+                </option>
+              </select>
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Code</label>
+              <input
+                type="number"
+                name="code"
+                placeholder="Code"
+                className="adminInput"
+              />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Description</label>
+              <input
+                type="text"
+                name="desc"
+                placeholder="Description"
+                className="adminInput"
+              />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Tags</label>
+              <input
+                type="text"
+                name="tags"
+                placeholder="Tags (seperated by comma)"
+                className="adminInput"
+              />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">BPM</label>
+              <input
+                type="text"
+                name="bpm"
+                placeholder="BPM"
+                className="adminInput"
+              />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Key</label>
+              <input
+                type="text"
+                name="key"
+                placeholder="Key"
+                className="adminInput"
+              />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Thumbnail</label>
+              <input type="file" name="thumbnail" className="adminInput" />
+            </div>
+            <div className="adminInputContainer">
+              <label className="adminInputLabel">Mp3</label>
+              <input type="file" name="mp3" className="adminInput" />
             </div>
           </div>
           <button className="bg-pink py-2 px-5 rounded mt-1">Submit</button>
