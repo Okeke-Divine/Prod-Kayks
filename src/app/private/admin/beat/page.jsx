eb594c86fcb4242d20f9ea323d0ccbaa77ff87fd
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 00:23:22 +0100

    Updated the beat list page

--- src/app/private/admin/beat/page.jsx
+++ src/app/private/admin/beat/page.jsx
@@ -5,7 +5,7 @@ export default function Beats() {
     <>
       <div className="mainLayout">
         <div className="flex flex-wrap gap-2 justify-between items-center">
-          <h1 className="text-2xl">Beat | New</h1>
+          <h1 className="text-2xl">Beat | List</h1>
           <Link
             href="/private/admin/beat/new"
             className="border border-pink bg-pink rounded py-1 px-2"
