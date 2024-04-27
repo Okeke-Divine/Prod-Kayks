354bdc4db812f39ac9ff741bd399ed7534dcfbfa
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-27 20:58:19 +0100

    Major update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Hero from "../components/home/Hero"
 import Services from "../components/home/Services"
 import Beats from "../components/home/Beats"
-import Genres from "../components/home/Genres"
+import Genres from "../components/home/Genres.tsx"
 
 export default function Home() {
   return (
--- src/components/home/Genres.tsx
+++ src/components/home/Genres.tsx
@@ -1,7 +1,15 @@
 import CTag from "../shared/CTag";
 import Divider from "../shared/Divider";
 
-export default function Genres() {
+import prisma from "../../db";
+
+async function getGenre() {
+  return await prisma.genre.findMany();
+}
+
+export default async function Genres() {
+  const genres = await getGenre();
+  console.log();
   return (
     <>
       <div className="mainLayout" id="beats">
@@ -9,8 +17,13 @@ export default function Genres() {
           <CTag name="Genres" />
         </div>
         <div className="mt-5 font-bold text-4xl text-center">Genres</div>
-
-        <div className="mb-5"></div>
+        <div className="mb-5">
+          {genres.map((genre, index) => (
+            <>
+              <div>{genre.name}</div>
+            </>
+          ))}
+        </div>
       </div>
       <br />
       <Divider />
