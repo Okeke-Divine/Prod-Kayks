a04825a1b476ac0d71afc90499d3b069c96ff1fc
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-27 21:22:21 +0100

    Updated

--- src/app/private/admin/layout.jsx
+++ src/app/private/admin/layout.jsx
@@ -1,7 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <>
-      <div>{children}</div>
+      <div className="flex gap-2">
+        <div className="py-10 px-3">Genre Beat</div>
+        <div>{children}</div>
+      </div>
     </>
   );
 }
