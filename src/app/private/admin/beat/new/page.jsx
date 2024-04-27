04dcf65c058100beb596edda9e47ed4091583e05
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 00:26:42 +0100

    Updated the beat form

--- src/app/private/admin/beat/new/page.jsx
+++ src/app/private/admin/beat/new/page.jsx
@@ -8,7 +8,12 @@ export default function NewBeat() {
     <>
       <div className="mainLayout">
         <h1 className="text-2xl">Beat | New</h1>
-        <form action={createBeat}></form>
+        <form action={createBeat} className="mt-5">
+            <div>
+                <div className="adminInputContainer"><input name="" className="adminInput" /></div>
+            </div>
+            <button className="bg-pink py-2 px-5 rounded mt-3">Submit</button>
+        </form>
       </div>
     </>
   );
