82cdc2da85b72ae37fecd6d191cd09ef77944a58
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 13:35:50 +0100

    uPDATED

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,15 @@ import Hero from "../components/home/Hero"
 import Services from "../components/home/Services"
 import Beats from "../components/home/Beats"
 import Genres from "../components/home/Genres"
+import { Suspense } from "react"
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Beats />
+      <Suspense fallback={<>Loading...</>}>
+        <Beats />
+      </Suspense>
       <Genres />
       <Services />
     </>
