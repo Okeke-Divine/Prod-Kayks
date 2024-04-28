4fa56654bbd7f829d9bc9586b44d11e4fc8de2a7
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 04:41:11 +0100

    Updated the beat coponent

--- src/components/home/BeatsByGenre.jsx
+++ src/components/home/BeatsByGenre.jsx
@@ -6,13 +6,28 @@ import ComponentTitle from "../shared/ComponentTitle";
 import BeatCard from "../shared/BeatCard";
 import prisma from "../../db";
 
+async function getBeatsByGenre(id) {
+  return await prisma.beat.findMany({ where: { genreId: id } });
+}
+
 export default async function BeatsByGenre({ genre }) {
+  const beats = await getBeatsByGenre(genre.id);
+
   return (
     <>
       <div className="mb-5">
         <ComponentTitle name={genre.name} url={`/beats/?genre=${genre.name}`} />
         <div className="beatFlex">
-
+          {beats.map((beat, index) => (
+            <div key={index}>
+              <BeatCard
+                title={beat.name}
+                description={beat.desc}
+                genre={genre.name}
+                imgUrl={beat.thumbnail}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </>
