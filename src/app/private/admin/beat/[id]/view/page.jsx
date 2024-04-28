1cb56a872da4f9c9a1313c497d7d9853eaa6903c
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 02:39:39 +0100

    Major update

--- src/app/private/admin/beat/[id]/view/page.jsx
+++ src/app/private/admin/beat/[id]/view/page.jsx
@@ -1,22 +1,31 @@
 import Link from "next/link";
 import prisma from "../../../../../../db";
 
-async function getBeat() {
+async function getBeat(id) {
   return await prisma.beat.findFirstOrThrow({ where: { id: id } });
 }
 
 export default async function ViewBeat({ params }) {
   const id = params.id;
-  const beat = await getBeat(id); // Use double equals for comparison
+  const beat = await getBeat(id);
+  console.log(beat);
   return (
     <>
       <div className="mainLayout">
         <div className="mb-10">
-          {beat.map((beat, index) => (
-            <div key={index}>
-              <div>Name: {beat.title}</div>
+            <div>
+              <div>Name: {beat.name}</div>
+              <div>Desc: {beat.desc}</div>
+              <div>Sold: {beat.sold}</div>
+              <div>Code: {beat.code}</div>
+              <div>Tags: {beat.tags}</div>
+              <div>Bpm: {beat.bpm}</div>
+              <div>Key: {beat.key}</div>
+              <div>Price: {beat.price}</div>
+              <div>Artist: {beat.artist}</div>
+              <div>Song: {beat.song}</div>
+              <div>Contract: {beat.sontract}</div>
             </div>
-          ))}
         </div>
         <Link href=".." className="bg-pink py-2 px-3 rounded">
           Back
