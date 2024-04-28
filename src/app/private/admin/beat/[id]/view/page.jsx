7d17715f9c67e2de89ddace43a4bed07f0bda77a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-04-28 02:36:47 +0100

    Major update

--- src/app/private/admin/beat/[id]/view/page.jsx
+++ src/app/private/admin/beat/[id]/view/page.jsx
@@ -1,35 +1,27 @@
 import Link from "next/link";
 import prisma from "../../../../../../db";
 
+async function getBeat() {
+  return await prisma.beat.findFirstOrThrow({ where: { id: id } });
+}
+
 export default async function ViewBeat({ params }) {
   const id = params.id;
-  try {
-    const beat = await prisma.beat.findFirstOrThrow({ where: { id: id } }); // Use double equals for comparison
-
-    // Display beat information here (e.g., title, artist, genre)
-    return (
-      <>
-        <div className="mainLayout">
-          <Link href=".." className="bg-pink py-2 px-3 rounded">
-            Back
-          </Link>
-          <h1>{beat.title}</h1>
-          {/* Add more elements to display other beat properties */}
+  const beat = await getBeat(id); // Use double equals for comparison
+  return (
+    <>
+      <div className="mainLayout">
+        <div className="mb-10">
+          {beat.map((beat, index) => (
+            <div key={index}>
+              <div>Name: {beat.title}</div>
+            </div>
+          ))}
         </div>
-      </>
-    );
-  } catch (error) {
-    console.error("Error fetching beat:", error);
-    // Optionally render an error message or fallback content here
-    return (
-      <>
-        <div className="mainLayout">
-          <p className="mb-10">Error: Beat not found.</p>
-          <Link href=".." className="bg-pink py-2 px-3 rounded">
-            Back
-          </Link>
-        </div>
-      </>
-    );
-  }
+        <Link href=".." className="bg-pink py-2 px-3 rounded">
+          Back
+        </Link>
+      </div>
+    </>
+  );
 }
