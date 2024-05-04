86e242a2fc9cd30c4b324b90bcaa9b7a42805739
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-04 09:19:24 +0100

    Major updates

--- src/components/shared/BeatCard.jsx
+++ src/components/shared/BeatCard.jsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState } from "react";
 import Link from "next/link";
 import CTag from "../shared/CTag";
 import Seperator from "../shared/Seperator";
@@ -61,7 +63,7 @@ export default function BeatCard({
             Buy
           </button>
           {free_download === true ? (
-            <Link href={mp3_url} target="_blank" download={true}>
+            <Link href={mp3_url} download={mp3_url}>
               <button className="bg-purple-dark-2 text-white py-2 px-3 text-sm rounded-lg cursor-pointer">
                 Free Download
               </button>
