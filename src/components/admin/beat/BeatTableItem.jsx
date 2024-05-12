1db07364dc5bc0c793a2ecdf9ec7ed55cccac742
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-12 18:16:11 +0100

    Updated the codebase

--- src/components/admin/beat/BeatTableItem.jsx
+++ src/components/admin/beat/BeatTableItem.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 export default function BeatTableItem({ beat, index }) {
   const viewUrl = `/private/admin/beat/${beat.id}/view`;
+  const editUrl = `/private/admin/beat/${beat.id}/edit`;
   return (
     <>
       <tr key={index}>
@@ -12,9 +13,12 @@ export default function BeatTableItem({ beat, index }) {
         <td className="adminTableRow">{beat.price}</td>
         <td className="adminTableRow">{beat.sold}</td>
         <td className="adminTableRow">{beat.code}</td>
-        <td className="adminTableRow">
+        <td className="adminTableRow flex gap-2">
           <Link className="text-wood" href={viewUrl}>
             View
+          </Link>
+          <Link className="text-wood" href={editUrl}>
+            Edit
           </Link>
         </td>
       </tr>
