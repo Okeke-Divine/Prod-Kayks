674a3e3cf0342dede9bbfa703cab552febbea9d5
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-05-12 18:57:43 +0100

    Force dynmaic test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,14 @@ import Hero from "../components/home/Hero";
 import Services from "../components/home/Services";
 import Beats from "../components/home/Beats";
 import Genres from "../components/home/Genres";
-import { Suspense } from "react";
+
+export const dynamic = 'force-dynamic'
 
 export default async function LandingPage() {
   return (
     <>
       <Hero />
-      {/* <Suspense fallback={<>Loading...</>}> */}
       <Beats />
-      {/* </Suspense> */}
       <Genres />
       <Services />
     </>
